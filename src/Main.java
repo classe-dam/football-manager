ac6a27c8eee172b24f07088ff5053bd1044356cf
j <joelpepa2@gmail.com>
2024-02-19 11:02:04 +0100

    update: Person.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import persons.Jugador;
 
 public class Main {
     public static void main(String[] args) {
+        Jugador jugador = new Jugador("nom",
+                "cognom",
+                "datanAix",
+                10000,
+                true,
+                5,
+                Jugador.Posicio.MIG
+                );
 
+        System.out.println(jugador.getPuntuacio());
+        jugador.entrenar();
+        System.out.println(jugador.getPuntuacio());
     }
 }
--- src/persons/Person.java
+++ src/persons/Person.java
@@ -1,4 +1,98 @@
 package persons;
 
+import utils.Utils;
+
 public abstract class Person {
+    private static int numPersones;
+
+    private int personaId;
+    private String nom;
+    private String cognom;
+    private String dataNaix;
+    private double nivellMotivacio; //1-10
+    private double puntuacio; //30-100
+
+    private float sou;
+
+    public void entrenar(){
+        this.nivellMotivacio += 2;
+        if(this.nivellMotivacio > 10){
+            this.nivellMotivacio = 10;
+        }
+    }
+
+    public Person(String nom, String cognom, String dataNaix, float sou) {
+        Person.numPersones += 1;
+        this.personaId = Person.numPersones;
+        this.nom = nom;
+        this.cognom = cognom;
+        this.dataNaix = dataNaix;
+        this.nivellMotivacio = 5;
+        this.sou = sou;
+        this.puntuacio = Utils.randomNumberBetween(30,100);
+    }
+
+    public float getSou() {
+        return sou;
+    }
+
+    public void setSou(float sou) {
+        this.sou = sou;
+    }
+
+    public static int getNumPersones() {
+        return numPersones;
+    }
+
+    public int getPersonaId() {
+        return personaId;
+    }
+
+    public void setPersonaId(int personaId) {
+        this.personaId = personaId;
+    }
+    public double getPuntuacio() {
+        return puntuacio;
+    }
+
+    public void setPuntuacio(double puntuacio) {
+        if(puntuacio > 100){
+            this.puntuacio = 100;
+        }else if (puntuacio < 30){
+            this.puntuacio = 30;
+        }else{
+            this.puntuacio = puntuacio;
+        }
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+
+    public String getCognom() {
+        return cognom;
+    }
+
+
+    public String getDataNaix() {
+        return dataNaix;
+    }
+
+
+    public double getNivellMotivacio() {
+        return nivellMotivacio;
+    }
+
+    public void setNivellMotivacio(double nivellMotivacio) {
+        this.nivellMotivacio = nivellMotivacio;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "personaId=" + personaId +
+                ", nom='" + nom + '\'' +
+                '}';
+    }
 }
